b79f413e86537a35fa14bfaaa222517c888005e1
Kalin Damyanov <kdamianov@gmail.com>
2023-11-13 16:28:14 +0200

    Added registrationUser Test, GreenMail dependency

--- src/test/java/org/softuni/mobilele/service/impl/CurrencyServiceImplTestIntegrTest.java
+++ src/test/java/org/softuni/mobilele/service/impl/CurrencyServiceImplTestIntegrTest.java
@@ -1,16 +1,110 @@
 package org.softuni.mobilele.service.impl;
 
+import net.bytebuddy.asm.MemberSubstitution;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.softuni.mobilele.model.dto.ExchangeRatesDTO;
+import org.softuni.mobilele.model.entity.ExchangeRateEntity;
+import org.softuni.mobilele.repository.ExchangeRateRepository;
 import org.softuni.mobilele.service.CurrencyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Stream;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 @SpringBootTest // вдига се целият app context
 class CurrencyServiceImplTestIntegrTest {
 
     @Autowired // injecting the real CurrencyService
     private CurrencyService currencyServiceToTest;
 
-    //TODO
+    @Autowired
+    private ExchangeRateRepository exchangeRateRepositoryToTest;
+
+    @BeforeEach
+    void setUp() {
+        exchangeRateRepositoryToTest.deleteAll();
+    }
+
+    @AfterEach
+    void tearDown() {
+        exchangeRateRepositoryToTest.deleteAll();
+    }
+    @ParameterizedTest(name = "Conversion BGN/USD exRate {0}, expected {1}")
+    //указваме кой метод да се ползва
+    @MethodSource("testDataBGN_TO_USD")
+    void test_BGN_TO_USD(Double exchangeRate, Double expectedValue) {
+        //    "base": "USD",
+        //    "rates": {
+        //      "BGN": 1.840515,
+        //      "EUR": 0.937668
+        //    }
+
+        // exp 0.54
+        var testExRate =
+                new ExchangeRatesDTO("USD", Map.of("BGN", BigDecimal.valueOf(exchangeRate)));
+
+        currencyServiceToTest.refreshRates(testExRate);
+
+        Optional<ExchangeRateEntity> exRateUSD_BGN = exchangeRateRepositoryToTest.findById("USD");
+
+        assertTrue(exRateUSD_BGN.isPresent());
+        assertEquals(
+                BigDecimal.valueOf(expectedValue).setScale(2, RoundingMode.DOWN),
+                exRateUSD_BGN.map(ExchangeRateEntity::getRate).orElseThrow());
+    }
+
+    @ParameterizedTest(name = "Conversion BGN/EUR exRateBGN {0}, exRateEUR {1}, expected {2}")
+    @MethodSource("test_Data_BGN_TO_EUR")
+    void testBGN_TO_EUR(Double exchangeRateBGN,
+                        Double exchangeRateEUR,
+                        Double expectedValue) {
+        //    "base": "USD",
+        //    "rates": {
+        //      "BGN": 1.840515,
+        //      "EUR": 0.937668
+        //    }
+
+        var testExRate =
+                new ExchangeRatesDTO("USD", Map.of("BGN", BigDecimal.valueOf(exchangeRateBGN),
+                        "EUR", BigDecimal.valueOf(exchangeRateEUR)));
+
+        currencyServiceToTest.refreshRates(testExRate);
+
+        Optional<ExchangeRateEntity> exRateUSD_BGN = exchangeRateRepositoryToTest.findById("EUR");
+
+        assertTrue(exRateUSD_BGN.isPresent());
+        assertEquals(
+                BigDecimal.valueOf(expectedValue).setScale(2, RoundingMode.DOWN),
+                exRateUSD_BGN.map(ExchangeRateEntity::getRate).orElseThrow());
+
+
+    }
+
+    private static Stream<Arguments> test_Data_BGN_TO_EUR() {
+        return Stream.of(
+                Arguments.of(1.840515, 0.937668, 0.51),
+                Arguments.of(1.777515, 0.544454, 0.31),
+                Arguments.of(1.0, 1.0, 1.0)
+        );
+    }
+
+    private static Stream<Arguments> testDataBGN_TO_USD() {
+        return Stream.of(
+                Arguments.of(1.840515, 0.54),
+                Arguments.of(1.752345, 0.57),
+                Arguments.of(1.333333, 0.75),
+                Arguments.of(1.0, 1.0)
+        );
+    }
 }
---
+++ src/test/java/org/softuni/mobilele/web/UserRegistrationControllerTestIntegrTest.java
@@ -0,0 +1,64 @@
+package org.softuni.mobilele.web;
+
+import com.icegreen.greenmail.util.GreenMail;
+import com.icegreen.greenmail.util.ServerSetup;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc //вдига се MOck-нат mvc
+class UserRegistrationControllerTestIntegrTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    //задаваме стойности на параметрите към GreenMail
+    @Value("${mail.port}")
+    private int port;
+
+    @Value("${mail.host}")
+    private String host;
+
+    @Value("${mail.username}")
+    private String username;
+
+    @Value("${mail.password}")
+    private String password;
+
+    private GreenMail greenMail;
+    @BeforeEach
+    void setUp() {
+        greenMail = new GreenMail(new ServerSetup(port, host,"smtp"));
+        greenMail.start();
+        greenMail.setUser(username, password);
+    }
+
+    @AfterEach
+    void tearDown() {
+        greenMail.stop();
+    }
+    @Test
+    void testRegistration() throws Exception {
+        mockMvc.perform(
+                //изпращаме POST заявка
+                MockMvcRequestBuilders.post("/users/register")
+                        .param("email", "pesho@softuni.bg")
+                        .param("firstName", "Pesho")
+                        .param("lastName", "Petrov")
+                        .param("password", "topsecret")
+                        .param("confirmPassword", "topsecret")
+                        .with(csrf())
+        ).andExpect(status().is3xxRedirection()); //redirect към /home
+    }
+
+}
